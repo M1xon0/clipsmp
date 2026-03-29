c676b857145c6c722581e5d10e39d7a5ea507a85
M1xon0 <cmi1.mikolaj@gmail.com>
2026-03-29 18:00:59 +0200

    Create main.js as the main Electron process file for the ClipSMP launcher

---
+++ main.js
@@ -0,0 +1,33 @@
+// main.js
+// This is the main Electron process file for the ClipSMP launcher.
+
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) createWindow();
+});
